c25d6139c9ddc7a185d8cb75701882533b5b825b
sabababluani <sababablo@gmail.com>
2025-04-08 19:22:32 +0400

    feat: added script tag in head

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -3,6 +3,7 @@ import "../globals.css";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import { Metadata } from "next";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "Cobbler",
@@ -26,18 +27,20 @@ export default async function LocaleLayout({
     <NextIntlClientProvider>
       <html lang={locale} className={fontClass}>
         <head>
-          <script
-            type="text/javascript"
+          <Script
+            id="clarity-script"
+            strategy="afterInteractive"
             dangerouslySetInnerHTML={{
-              __html: `(function(c,l,a,r,i,t,y){
-        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-      })(window, document, "clarity", "script", "r0k2siof4x");`,
+              __html: `
+              (function(c,l,a,r,i,t,y){
+                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "r0k2siof4x");
+            `,
             }}
           />
         </head>
-
         <body>
           <Header />
           {children}
